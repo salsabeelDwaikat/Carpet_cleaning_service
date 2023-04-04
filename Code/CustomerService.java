19f3b12fc9cf7b5f6c04846e4c574afb0c46eb83
doaa-samad <s11924191@stu.najah.edu>
2023-04-04 15:41:17 +0200

    3:41pm 4/4

--- Code/CustomerService.java
+++ Code/CustomerService.java
@@ -1,11 +1,15 @@
+
 package Code;
 
 import java.util.ArrayList;
 
 public class CustomerService {
-
-
-	
+	static Customer c1;
+	static Customer c2;
+	static Customer c3;
+	static Product p1;
+	static Product p2;
+	static Product p3;
 	 static ArrayList<Customer> customs= new ArrayList<Customer>();	
 	
 	
@@ -20,19 +24,19 @@ public class CustomerService {
 	}
 	
 	public static void AddUsersFirstTime() {
-		Customer c1=new Customer("144","aya","aya123","0598764123","Nablus,aldwar");
-		Customer c2=new Customer("176","naser yonis","nas234","0563214698","Nablus,25th street");
-		Customer c3=new Customer("123","leen ayman","leenay123","0593587456","rafidya street");
-		Product p1= new Product("Carpet","wool","2m","3m","Stain removal");
-		Product p2= new Product("Carpet","furr","4.5m","3.2m","children safe cleaning");
-		Product p3= new Product("mattress","cotton","200cm","280cm","Stain removal");
+		c1=new Customer("144","aya","aya123","0598764123","Nablus aldwar");
+		c2=new Customer("176","naser yonis","nas234","0563214698","Nablus,25th street");
+		c3=new Customer("123","leen ayman","leenay123","0593587456","rafidya");
+		p1= new Product("Carpet","wool","2m","3m","Stain removal");
+		p2= new Product("Carpet","furr","4.5m","3.2m","children safe cleaning");
+		p3= new Product("mattress","cotton","200cm","280cm","Stain removal");
 		
 		customs.add(c1);
 		customs.add(c2);
 		customs.add(c3);
-		customs.get(0).List.add(p1);
-		customs.get(1).List.add(p2);
-		customs.get(2).List.add(p3);
+		c1.List.add(p1);
+		c2.List.add(p2);
+		c3.List.add(p3);
 		
 	}
 	
@@ -49,14 +53,15 @@ public class CustomerService {
 	
 	public static boolean UserList(String username, String type, String special) {
 		int y=customerInt(username);
-		
 				for(int k=0;k<customs.get(y).List.size();k++) {
 					if(customs.get(y).List.get(k).getProdType().equalsIgnoreCase(type)) {
-					
-						if(customs.get(y).List.get(k).getSpecialTreat().equalsIgnoreCase(special))
-							return true;}
+						
+						if(customs.get(0).List.get(k).getSpecialTreat().equalsIgnoreCase(special)) 
+							return false;
+
+						}
 				}
-      return false;
+      return true;
 	}
 	
 	
@@ -78,6 +83,26 @@ int x = 0;
 			
 	}
 	
+   public static int pList(String user){
+		for(int i=0;i<customs.size();i++) {
+			if(customs.get(i).getName().equalsIgnoreCase(user))
+				return customs.get(i).List.size();
+
+		}
+		return -1;
+	}
+   
+   public static String userAddress(String username) {
+	   int k=-1;
+	   k=customerInt(username);
+	   return customs.get(k).getAddress();
+
+   }
 	
+   public static ArrayList<Product> allprod(String username){
+	   int y = customerInt(username);
+	   return customs.get(y).List;
+   }
+
 }
 
