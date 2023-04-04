c272d8bc4b1069c7b9c1e131f841e64a6237915a
doaa-samad <s11924191@stu.najah.edu>
2023-04-04 15:40:38 +0200

    3:40pm 4/4

--- Code/Adminservice.java
+++ Code/Adminservice.java
@@ -6,9 +6,33 @@ public class Adminservice {
 
 	static ArrayList<String> service=new ArrayList<String>();
 	static ArrayList<String> price=new ArrayList<String>();
-	 static private ArrayList<Admin> admn=new ArrayList<Admin>();
+	static ArrayList<String>address= new ArrayList<String>();
+	static ArrayList<String>addPrice= new ArrayList<String>();
+	static private ArrayList<Admin> admn=new ArrayList<Admin>();
 
-
+    public static void AddLocationNdPrice() {
+    	address.add("Nablus aldwar");
+    	address.add("Nablus,25th street");
+    	address.add("rafidya street");
+    	address.add("AlaaAldeen street");
+    	address.add("almakhfi");
+    	addPrice.add("20");
+    	addPrice.add("25");
+    	addPrice.add("15");
+    	addPrice.add("10");
+    	addPrice.add("20");
+    	addPrice.add("12");
+    }
+    
+    public static void Addelemnts() {
+    	Adminservice.service.add("Steam Cleaning");
+		Adminservice.service.add("Stain removal");
+		Adminservice.service.add("Children safe cleaning");
+		Adminservice.price.add("50");
+		Adminservice.price.add("100");
+		Adminservice.price.add("250");
+		
+    }
 
 	public static boolean checkLogin(String u) {
 
@@ -39,8 +63,15 @@ public class Adminservice {
 	     return false;	
 
 	}	
+public static String deliveryAvaialble(String add) {
+	for(int i=0;i<address.size();i++){
+		if(address.get(i).equalsIgnoreCase(add)) {
+			return addPrice.get(i);
+		}
+	}
 
-
+	return null;
+}
 
 
 }
